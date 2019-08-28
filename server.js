90c060197eb0c6067d634401323b8c0ab99f9352
ppatel662 <phpatel662@gmail.com>
2019-08-28 14:32:12 -0400

    final

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://payal:123456a@ds353007.mlab.com:53007/heroku_0qh3mtl5";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password1@ds259207.mlab.com:59207/heroku_c8zlspr1";
 mongoose.connect(MONGODB_URI);
 
 
